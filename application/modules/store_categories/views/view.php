6a024a91f315931872d2da8f8c463af725bfefe6
choutola <tola@placepass.com>
2017-08-17 09:45:30 +0700

    fd

--- application/modules/store_categories/views/view.php
+++ application/modules/store_categories/views/view.php
@@ -9,11 +9,12 @@ $item_price = $row->item_price;
 $small_pic_path = base_url()."small_pics/".$small_pic;
 $item_page = base_url().$item_segments."/".$row->item_url;
 ?>
-<div class="col-md-2 img-thumbnail" style="margin: 6px; height: 300px;">
+<div class="col-md-2 img-thumbnail" style="margin: 6px; height: 350px;">
 	<a href="<?= $item_page ?>"><img src="<?= $small_pic_path ?>" alt="<?= $item_title ?>" class="img-responsive"></a>
 	<br>
 	<h6><a href="<?= $item_page ?>"><?= $item_title  ?></a></h6>
-	<div style="clear: both; color:red; font-weight: bold;"><?= $dollar_symbol.number_format($item_price,2) ?>
+	<div style="clear: both; color:red; font-weight: bold;"><?= $dollar_symbol.number_format($item_price,2) ?>.0
+	<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
 	
 	</div>
 </div>
@@ -21,3 +22,78 @@ $item_page = base_url().$item_segments."/".$row->item_url;
 <?php
 }
 ?>
+<div style="clear: both;"></div>
+
+<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<div class="left-sidebar">
+						<h2>Category</h2>
+						<div class="panel-group category-products" id="accordian"><!--category-productsr-->
+							
+							
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#womens">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Womens
+										</a>
+									</h4>
+								</div>
+								<div id="womens" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Fendi</a></li>
+											<li><a href="#">Guess</a></li>
+											<li><a href="#">Valentino</a></li>
+											<li><a href="#">Dior</a></li>
+											<li><a href="#">Versace</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a href="#">Kids</a></h4>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a href="#">Fashion</a></h4>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a href="#">Households</a></h4>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a href="#">Interiors</a></h4>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a href="#">Clothing</a></h4>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a href="#">Bags</a></h4>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a href="#">Shoes</a></h4>
+								</div>
+							</div>
+						</div><!--/category-products-->
+						
+					</div>
+				</div>
+				</div>
+				</div>
+				</section>
+				
